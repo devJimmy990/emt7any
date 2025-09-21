d17d28ab773d64bb981f931f69fa62940c63ad82
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-09-21 23:43:10 +0300

    feat: create endpoints class

---
+++ lib/core/constants/end_points.dart
@@ -0,0 +1,27 @@
+class EndPoints {
+  static const String login = 'api/v1/auth/login';
+
+  static const String me = 'api/v1/me';
+
+  static const String studentDashboard = 'api/v1/student/dashboard';
+}
+
+/**
+ * 
+POST - api/v1/admin/create-school-admin
+POST - api/v1/admin/create-student
+POST - api/v1/admin/create-teacher
+
+POST - api/v1/auth/login
+POST - api/v1/logout
+GET - api/v1/me
+
+GET - api/v1/student/dashboard
+GET - api/v1/teacher/dashboard
+
+GET - sanctum/csrf-cookie
+GET - /
+GET - api/health
+GET - up
+
+ */
