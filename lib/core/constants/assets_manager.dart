2f7c3ed09d803a127abf83dd05ea5d6aa4a27480
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-09-30 23:50:16 +0300

    update packages, assets and readme file

--- lib/core/constants/assets_manager.dart
+++ lib/core/constants/assets_manager.dart
@@ -1,17 +1,12 @@
 // ignore_for_file: prefer_single_quotes
 class AssetsManager {
   AssetsManager._();
-  
+
   /// Assets for assetsImagesLogo
   /// assets/images/logo.png
   static const String assetsImagesLogo = "assets/images/logo.png";
-
-  /// Assets for assetsImagesQrCode
-  /// assets/images/qr_code.png
-  static const String assetsImagesQrCode = "assets/images/qr_code.png";
 
   /// Assets for assetsImagesUser
   /// assets/images/user.jpg
   static const String assetsImagesUser = "assets/images/user.jpg";
 }
-
